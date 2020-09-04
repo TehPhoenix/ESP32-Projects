99b0271b197a6b585132b8fb96be1a737ad86dcb
Paul <pc7865@yahoo.co.uk>
2020-09-05 00:49:35 +0100

    Time added to Garage Open Notification.

---
+++ GarageDoorController/src/AUTH_TOKEN.h
@@ -0,0 +1,9 @@
+    /*
+    AUTH_TOKEN.h
+
+    Store private tokens in AUTH_TOKEN.h and add that
+    file to .gitignore so personal data is not shared
+
+    */
+
+    #define BLYNK_AUTH_TOKEN "fdabbcd2fcf744cb99687befb31bf774"
