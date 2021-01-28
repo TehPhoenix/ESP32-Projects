64c3b394cc628e7bd94267eb5e00e3824aa6dc24
Paul <pc7865@yahoo.co.uk>
2021-01-28 01:09:16 +0000

    X

---
+++ Bird Counter/src/AUTH_TOKEN.h
@@ -0,0 +1,9 @@
+    /*
+    AUTH_TOKEN.h
+
+    Store private tokens in AUTH_TOKEN.h and add that
+    file to .gitignore so personal data is not shared
+
+    */
+
+    #define BLYNK_AUTH_TOKEN "bJXoWc2NQMXq5CQ1b2ar5n6Jk69jbToH"
